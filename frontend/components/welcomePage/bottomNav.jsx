eb843ec3778c4073c929648d3ca94e22430403b7
malans <marcusegues@gmail.com>
2016-06-09 23:34:56 +0200

    Add logo to nav bar on welcome page

--- frontend/components/welcomePage/bottomNav.jsx
+++ frontend/components/welcomePage/bottomNav.jsx
@@ -8,10 +8,14 @@ var BottomNav = React.createClass({
   render: function() {
     return (
       <header id="bottomNav">
-        <h1><a href="index.html"><strong>SF</strong> Swiss Feeds re</a></h1>
+        <h1>
+          <img id="welcomeAvatar" src="http://res.cloudinary.com/dolgs87zk/image/upload/v1465478807/SFLogo_jscwfx.jpg">
+          </img>
+          <span><strong>Swiss Feeds.</strong> Read and Discover Switzerland.</span>
+        </h1>
         <nav>
           <ul>
-              <li><a href="#" className="icon fa-info-circle">About</a></li>
+              <li><a href="#">About</a></li>
               <li><a onClick={this.openSignInForm}>Sign In</a></li>
           </ul>
         </nav>
